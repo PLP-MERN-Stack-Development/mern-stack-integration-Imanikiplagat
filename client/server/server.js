e6600ae128a685d85c6c13b6a1819d4b72ec679f
IMANI <kiplagatfaith88@gmail.com>
2026-01-13 11:15:07 +0300

    renaming for case sensitivity

--- client/server/server.js
+++ client/server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // API routes
-app.use('/api/mineis', postRoutes);
+app.use('/api/posts', postRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/auth', authRoutes);
 
