6b13720bce99d01fba602acab6e95271c73cf279
IMANI <kiplagatfaith88@gmail.com>
2026-01-13 06:48:24 +0300

    update CORS

--- client/server/server.js
+++ client/server/server.js
@@ -21,7 +21,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ 
-  origin: "http://localhost:5173",
+  origin: [ "http://localhost:5173",
+   " https://imani-blogpost.onrender.com "],
+
   credentials: true,
 }));
 app.use(express.json());
